8a89634bd4af8dc72e0179ab60d7bff889a1b5a4
MirageSoftware <ian@miragesoftware.co.uk>
2020-02-17 11:59:59 +0000

    Removed unnessacry try/catch

--- SavegameToolkit/ArkSavegame.cs
+++ SavegameToolkit/ArkSavegame.cs
@@ -95,33 +95,30 @@ namespace SavegameToolkit {
             //Now parse out cryo creature data
             foreach (var cryo in this.Objects.Where(x => x.ClassName.ToString().Contains("Cryop")).ToList())
             {
-                try
+   
+                var contents = ((((((((Structs.StructPropertyList)((cryo.Properties[4] as Propertys.PropertyArray).Value as Arrays.ArkArrayStruct)[0]).Properties[6] as Propertys.PropertyStruct)
+                .Value as Structs.StructPropertyList).Properties[0] as Propertys.PropertyArray).Value as Arrays.ArkArrayStruct)[0] as Structs.StructPropertyList).Properties[0] as Propertys.PropertyArray).Value as Arrays.ArkArrayUInt8;
+
+                var cryoStream = new System.IO.MemoryStream(contents.ToArray<Byte>());
+
+                using (ArkArchive cryoArchive = new ArkArchive(cryoStream))
                 {
-                    var contents = ((((((((Structs.StructPropertyList)((cryo.Properties[4] as Propertys.PropertyArray).Value as Arrays.ArkArrayStruct)[0]).Properties[6] as Propertys.PropertyStruct)
-                    .Value as Structs.StructPropertyList).Properties[0] as Propertys.PropertyArray).Value as Arrays.ArkArrayStruct)[0] as Structs.StructPropertyList).Properties[0] as Propertys.PropertyArray).Value as Arrays.ArkArrayUInt8;
+                    cryoArchive.ReadBytes(4);
+                    var dino = new GameObject(cryoArchive);
+                    var statusobject = new GameObject(cryoArchive);
+                    dino.LoadProperties(cryoArchive, new GameObject(), 0);
+                    statusobject.LoadProperties(cryoArchive, new GameObject(), 0);
+                    dino.IsCryo = true;
 
-                    var cryoStream = new System.IO.MemoryStream(contents.ToArray<Byte>());
+                    addObject(dino, true);
+                    addObject(statusobject, true);
 
-                    using (ArkArchive cryoArchive = new ArkArchive(cryoStream))
-                    {
-                        cryoArchive.ReadBytes(4);
-                        var dino = new GameObject(cryoArchive);
-                        var statusobject = new GameObject(cryoArchive);
-                        dino.LoadProperties(cryoArchive, new GameObject(), 0);
-                        statusobject.LoadProperties(cryoArchive, new GameObject(), 0);
-                        dino.IsCryo = true;
+                    //hack the id's so that the dino points to the appropriate dinostatuscomponent
+                    var statusComponentRef = dino.GetTypedProperty<PropertyObject>("MyCharacterStatusComponent");
+                    statusComponentRef.Value.ObjectId = statusobject.Id;
 
-                        addObject(dino, true);
-                        addObject(statusobject, true);
-
-                        //hack the id's so that the dino points to the appropriate dinostatuscomponent
-                        var statusComponentRef = dino.GetTypedProperty<PropertyObject>("MyCharacterStatusComponent");
-                        statusComponentRef.Value.ObjectId = statusobject.Id;
-
-                    }
                 }
-                catch (Exception)
-                { }
+
             }
                                 
 
